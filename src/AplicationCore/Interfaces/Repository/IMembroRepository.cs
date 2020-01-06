a00e5d2bbfa9b4867d721919d86328ca662bab23
Cicero Cavalcanti <cvictorj17@gmail.com>
2020-01-06 17:25:02 -0300

    Membro Repositorry

---
+++ src/AplicationCore/Interfaces/Repository/IMembroRepository.cs
@@ -0,0 +1,12 @@
+﻿using AplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicationCore.Interfaces.Repository
+{
+    public interface IMembroRepository : IBaseRepository<Membro>
+    {
+        IEnumerable<Membro> ObterPorProfissao(string profissao);
+    }
+}
---
+++ src/Infrastructure/Repository/MembroRepository.cs
@@ -0,0 +1,23 @@
+﻿using AplicationCore.Entity;
+using AplicationCore.Interfaces.Repository;
+using Infrastructure.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class MembroRepository : EFRepository<Membro>, IMembroRepository
+    {
+        public MembroRepository(ContextVictorious dbContext): base(dbContext)
+        {
+
+        }
+
+        public IEnumerable<Membro> ObterPorProfissao(string profissao)
+        {
+            return Buscar(x => x.Profissao == profissao);
+        }
+    }
+}
